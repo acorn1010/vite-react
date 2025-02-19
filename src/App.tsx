3be6caeee65051a0296b7d8a94f4780c54178956
Acorn <github@unallied.com>
2025-02-18 16:21:02 -0800

    Add test image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import testImage from './assets/testImage.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -15,6 +16,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <img src={testImage} className="logo" alt="Test Image" />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
